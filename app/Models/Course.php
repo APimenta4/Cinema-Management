e848509fbb0afd9ff53cd06e4b1cd7e6bd4eb45d
Afonso Pimenta <afonsocpimenta04@gmail.com>
2024-05-25 16:10:21 +0100

    dsmaskdma

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -12,16 +12,7 @@ class Course extends Model
     use HasFactory;
 
     protected $fillable = [
-        'abbreviation',
-        'name',
-        'name_pt',
-        'type',
-        'semesters',
-        'ECTS',
-        'places',
-        'contact',
-        'objectives',
-        'objectives_pt',
+        
     ];
 
     public $timestamps = false;
